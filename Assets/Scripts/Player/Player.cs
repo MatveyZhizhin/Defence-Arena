8a314b39658ae454038ee2613564c98aebacf931
MatveyZhizhin <117218670+MatveyZhizhin@users.noreply.github.com>
2023-07-16 01:45:19 +0300

    Изменения названия скрипта
    Изменено название скрипта PlayerMovement на Player

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Assets.Scripts.Player
 {
     [RequireComponent(typeof(Rigidbody))]
-    public class PlayerMovement : MonoBehaviour
+    public class Player : MonoBehaviour
     {
         [SerializeField] private float speed;
         private Rigidbody playerRigidbody;
