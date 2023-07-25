b769ac3c099bdc55ff4875a366072a89a6215871
MatveyZhizhin <117218670+MatveyZhizhin@users.noreply.github.com>
2023-07-25 22:22:00 +0300

    Изменение скрипта дробовика

--- Assets/Scripts/Player/Guns/ShotGun.cs
+++ Assets/Scripts/Player/Guns/ShotGun.cs
@@ -1,22 +1,20 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace Assets.Scripts.Player.Guns
 {
-    public class ShotGun : Gun
+    public class Shotgun : Gun
     {
-        [SerializeField] private Transform firePoint1;
-        [SerializeField] private Transform firePoint2;
-        [SerializeField] private Transform firePoint3;
+        private Transform[] firePoints;
 
         protected override IEnumerator Fire()
         {
             while (true)
             {
-                Instantiate(bullet, firePoint1.position, firePoint1.rotation);
-                Instantiate(bullet, firePoint2.position, firePoint2.rotation);
-                Instantiate(bullet, firePoint3.position, firePoint3.rotation);
+                foreach (var firePoint in firePoints)
+                {
+                    Instantiate(bullet, firePoint.position, firePoint.rotation);
+                }
                 yield return new WaitForSeconds(fireRate);
             }
         }
