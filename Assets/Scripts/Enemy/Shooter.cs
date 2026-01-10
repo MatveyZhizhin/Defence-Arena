28bad988bc29c81aaf77fc0cdd6b0dec29d670f1
MatveyZhizhin <117218670+MatveyZhizhin@users.noreply.github.com>
2026-01-10 22:54:14 +0300

    Изменил атаку сове

--- Assets/Scripts/Enemy/Shooter.cs
+++ Assets/Scripts/Enemy/Shooter.cs
@@ -6,12 +6,15 @@ namespace Assets.Scripts.Enemy
     public class Shooter : _Enemy
     {        
         [SerializeField] private _Bullet bullet;
-        [SerializeField] private Transform firePoint;     
+        [SerializeField] private Transform[] firePoints;     
         
         private void Fire()
         {
-            var newBullet = Instantiate(bullet, firePoint.position, transform.rotation);
-            newBullet.SetDamage(damage);
+            foreach (var firePoint in firePoints)
+            {
+                var newBullet = Instantiate(bullet, firePoint.position, firePoint.rotation);
+                newBullet.SetDamage(damage);
+            }                   
         }
 
         protected override IEnumerator Attack()
