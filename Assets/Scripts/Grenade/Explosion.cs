f6c68d39492ac205aff7123662cfed727877587d
Krasney5 <137995759+Krasney5@users.noreply.github.com>
2026-01-14 23:15:24 +0300

    granade

---
+++ Assets/Scripts/Grenade/Explosion.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using Assets.Scripts.Enemy;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    [SerializeField] private float _radius;
+    [SerializeField] private float _damage;
+    [SerializeField] private float _timeBeforeTheExplosion;
+    [SerializeField] private AudioSource _audioSources;
+    [SerializeField] private ParticleSystem _particleSystem;
+    [SerializeField]private GameObject _granate;
+
+    private void OnEnable()
+    {
+        StartCoroutine(Explode());
+    }
+
+    private IEnumerator Explode()
+    {
+        yield return new WaitForSeconds(_timeBeforeTheExplosion);
+        Collider[] hits = Physics.OverlapSphere(transform.position, _radius);
+        _audioSources.Play();
+        _particleSystem.Play();
+        foreach (var hit in hits)
+        {
+            if (hit.TryGetComponent(out _Enemy enemy))
+            {
+                enemy.TakeDamage(_damage);
+            }
+        }
+        Destroy(_granate);
+        Destroy(gameObject, 5);
+    }
+}
